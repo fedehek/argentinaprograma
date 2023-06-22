9bfbec47c2ab10914e2cfd025e61c5614241c997
Federico Hekimian <fede.hekimian@gmail.com>
2023-06-22 20:28:44 -0300

    reordenado

--- multiplicacion.js
+++
@@ -1,5 +0,0 @@
-let val1 = 2;
-let val2 = 17;
-let val3 = 6;
-let multiplicacion = val1 * val2 * val3;
-console.log(multiplicacion);
--- resta.js
+++
@@ -1,4 +0,0 @@
-let num1 = 10;
-let num2 = 5;
-let resta = num1 - num2;
-console.log(resta);
